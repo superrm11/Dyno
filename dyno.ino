660624fc1b0673322e1a74b59d6b7c06d2cea2c8
Ryan McGee <ramtech51@comcast.net>
2022-04-13 16:45:34 -0400

    First working build

--- dyno.ino
+++ dyno.ino
@@ -8,10 +8,10 @@
 #define VOLT_SENSE A7
 #define CURR_SENSE A6
 
-#define ENC_PPR 8
+#define ENC_PPR 48
 
 
-Encoder enc(ENC_A, ENC_B);
+Encoder enc(ENC_B, ENC_A);
 HX711 load;
 
 void setup()
@@ -24,11 +24,23 @@ void setup()
 
     load.begin(LOADCELL_DOUT, LOADCELL_CLK);
     load.set_scale(219);
-    // load.set_scale();
     load.tare();
-
+    delay(100);
 
     Serial.begin(9600);    
+
+    // return;
+    Serial.println("TIME\tLOAD\tSPEED\tVOLT\tCURRENT\tTORQUE\tMOTOR_POWER\tELEC_POWER\tEFFICIENCY\n");
+
+    set_motor(100);
+    while(millis() < 10000)
+    {
+        print_data();
+        // delay(20);
+    }
+
+    
+
 }
 
 void set_motor(int pct)
@@ -36,11 +48,15 @@ void set_motor(int pct)
     if (pct > 0)
     {
         analogWrite(5, pct * 255 / 100);
-        analogWrite(6, 0);
-    } else
+        digitalWrite(6, LOW);
+    } else if (pct < 0)
     {
         analogWrite(6, -pct * 255 / 100);
-        analogWrite(5, 0);
+        digitalWrite(5, LOW);
+    } else
+    {
+        digitalWrite(5, LOW);
+        digitalWrite(6, LOW);
     }
 
 }
@@ -68,25 +84,70 @@ double get_voltage()
 
 double get_current()
 {
-    return analogRead(CURR_SENSE);
+    return (analogRead(CURR_SENSE) * -0.0256) + 13;
+}
+
+double get_force()
+{
+    return load.get_units() * 0.00220462; // milli-pounds
+}
+
+double get_torque()
+{
+    return get_force() * 5.5; // 5.5 inch lever arm
 }
 
 void print_data()
 {
+    double force = get_force();
+    double rpm = get_rpm();
+    double voltage = get_voltage();
+    double current = get_current();
+    double torque = get_torque();
+    double motor_power = torque * (rpm / 60) * 1.36;
+    double elec_power = current * voltage;
+    double efficiency = motor_power / elec_power;
+
     Serial.print(millis() / 1000.0);
     Serial.print(", ");
-    Serial.print(load.get_units());
+    Serial.print(force, 4);
     Serial.print(", ");
-    Serial.print(get_rpm());
+    Serial.print(rpm);
     Serial.print(", ");
-    Serial.print(get_voltage());
+    Serial.print(voltage, 4);
     Serial.print(", ");
-    Serial.println(get_current());
+    Serial.print(current, 4);
+    Serial.print(", ");
+    Serial.print(torque, 4);
+    Serial.print(", ");
+    Serial.print(motor_power, 4);
+    Serial.print(", ");
+    Serial.print(elec_power, 4);
+    Serial.print(", ");
+    Serial.println(efficiency, 4);
 }
 
 void loop()
 {    
-    Serial.println(analogRead(CURR_SENSE));
-    set_motor(100);
+
+    set_motor(0);
+
+    // Serial.println(get_force(), 3);
+    
+    // Serial.println(enc.read());
+
+    // Serial.println(enc.read());
+
+
+    // Serial.println(get_current());
+
+    // if((millis() / 10000) % 2 == 0)
+    // {
+    //     set_motor(100);
+    // } else
+    // {
+    //     set_motor(0);
+    // }
+
     delay(20);
 }
